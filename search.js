3ef26b9da1b80a5c862158a30cd713540846fc00
Debajyati <ddebajyati@gmail.com>
2024-06-28 01:14:36 +0530

    improved UI related bugs. Added a search bar. minimised the animation to fit in the block

--- search.js
+++ search.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const searchBar = document.getElementById("search-bar");
   const playlist = document.getElementById("playlist");
-  let tracks = Array.from(playlist.getElementsByTagName("li"));
 
   searchBar.addEventListener("input", () => {
     const query = searchBar.value.toLowerCase();
+    let tracks = Array.from(playlist.getElementsByTagName("li"));
     tracks.forEach(track => {
       const text = track.textContent.toLowerCase();
       if (text.includes(query)) {
